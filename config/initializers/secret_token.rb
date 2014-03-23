838763735509b15ab7d79935fbcdda690dfc25cc
Andrew Namakarskiy <anamakarskiy@gmail.com>
2014-03-23 17:35:34 +0300

    First version of start page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelpMeFind::Application.config.secret_key_base = '54017b68f4b63e10130ad61007fc1406794162f8f506d5ae23791a3d4a00f0fbd09d27cfb3a3db44a526796c7dffbf02ffbec34214dc1a95a1a8c4e15705470b'
